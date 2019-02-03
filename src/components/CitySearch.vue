01b9e8759d6aaa9ae954ccd54f57db53d5dad92d
taraschuiko <taraschuiko@gmail.com>
2019-02-03 13:18:18 +0200

    Made weather getting

--- src/components/CitySearch.vue
+++ src/components/CitySearch.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <input type="text" placeholder="Search...">
+    <input type="text" placeholder="Search..." v-model="newCity">
+    <button @click="loadWeather">Get Weather</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "CitySearch"
+  name: "CitySearch",
+  data() {
+    return {
+      newCity: ""
+    };
+  },
+  methods: {
+    loadWeather() {
+      this.$store.dispatch("loadWeather", this.newCity);
+      this.newCity = "";
+    }
+  }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -3,14 +3,42 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const APP_ID = "appid=34260ce975aea61ded7b1014aecb5d54"
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?${APP_ID}`
+
 export default new Vuex.Store({
   state: {
-
+    city: "London",
+    units: "metric",
+    weather: {
+      main: "",
+      temp: 0,
+      windSpeed: 0
+    }
   },
   mutations: {
-
+    setCity(state, city) {
+      state.city = city;
+    },
+    setUnits(state, units) {
+      state.units = units;
+    },
+    setWeather(state, weather) {
+      state.weather = weather;
+    }
   },
   actions: {
-
+    loadWeather(context, city) {
+      context.commit("setCity", city);
+      let weather = {};
+      fetch(BASE_URL + "&q=" + this.state.city + "&units=" + this.state.units)
+        .then(r => r.json())
+        .then(json => weather = {
+          main: json.weather[0].main,
+          temp: json.main.temp,
+          windSpeed: json.wind.speed
+        })
+        .then(() => context.commit("setWeather", weather))
+    }
   }
 })
